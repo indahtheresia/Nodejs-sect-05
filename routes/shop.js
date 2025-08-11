f85ff910ebf6f853ca30589888fb9feb5bdf9309
indahtheresia <indahtheresiatheh29@gmail.com>
2025-08-11 12:25:58 +0800

    sharing data across requests & users

--- routes/shop.js
+++ routes/shop.js
@@ -2,11 +2,14 @@ const path = require('path');
 
 const express = require('express');
 
+const adminData = require('./admin');
+
 const rootDir = require('../util/path');
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
+  console.log(adminData.product);
   res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
