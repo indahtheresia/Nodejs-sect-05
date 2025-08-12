94b7ffc332099785275aad7133ce68cdca939ea2
indahtheresia <indahtheresiatheh29@gmail.com>
2025-08-12 12:26:30 +0800

    finishing the controllers

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const page404Controllers = require('./controllers/404');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
@@ -17,9 +18,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-  res.status(404).render('404', { title: 'Not Found', path: '' })
-})
+app.use(page404Controllers.get404);
 
 app.listen(3000);
