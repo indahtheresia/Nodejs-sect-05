142657fe4eb9c7cdd386f958f318a254aec2d28f
indahtheresia <indahtheresiatheh29@gmail.com>
2025-08-15 14:40:40 +0800

    creating the shop structure

--- controllers/products.js
+++ controllers/products.js
@@ -2,7 +2,7 @@ const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
   // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-  res.render('add-product', { title: 'Add Product', path:'/admin/add-product', formsCSS: true, productCSS: true, addProductActive: true });
+  res.render('admin/add-product', { title: 'Add Product', path:'/admin/add-product', formsCSS: true, productCSS: true, addProductActive: true });
 }
 
 exports.postAddProduct = (req, res, next) => {
@@ -12,6 +12,6 @@ exports.postAddProduct = (req, res, next) => {
 }
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => res.render('shop', { prods: products, title: 'Shop', path: '/', hasProducts: products.length > 0, productCSS: true, shopActive: true }));
+  Product.fetchAll(products => res.render('shop/product-list', { prods: products, title: 'Shop', path: '/', hasProducts: products.length > 0, productCSS: true, shopActive: true }));
   ;
 }
