08654e63c1149598a564933d30b40b05af52982e
indahtheresia <indahtheresiatheh29@gmail.com>
2025-08-20 14:57:26 +0800

    inserting data into the database

--- models/product.js
+++ models/product.js
@@ -10,7 +10,7 @@ module.exports = class Product{
   }
 
   save() {
-    
+    return db.execute('INSERT INTO products (title, imageUrl, price, description) VALUES (?, ?, ?, ?)', [this.title, this.imageUrl, this.price, this.description]);
   }
 
   static deleteById(id) {
