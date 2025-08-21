08edadd3f2d824b8c80ba90dfeab2d527a68ea44
indahtheresia <indahtheresiatheh29@gmail.com>
2025-08-21 15:15:34 +0800

    fetching admin products

--- controllers/admin.js
+++ controllers/admin.js
@@ -44,7 +44,9 @@ exports.postEditProduct = (req, res, next) => {
 }
 
 exports.getAdminProducts = (req, res, next) => {
-  Product.fetchAll(products => res.render('admin/products', { prods: products, title: 'Admin Products', path: '/admin/products' }));
+  Product.findAll().then(products => {
+    res.render('admin/products', { prods: products, title: 'Admin Products', path: '/admin/products' })
+  }).catch(err => console.log(err));
 }
 
 exports.postDeleteProduct = (req, res, next) => {
