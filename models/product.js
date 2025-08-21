089b8731555a305d8fa002dbf21228a3e56a90fb
indahtheresia <indahtheresiatheh29@gmail.com>
2025-08-21 12:49:32 +0800

    defining a model

--- models/product.js
+++ models/product.js
@@ -1,27 +1,27 @@
-const Cart = require('./cart');
-const db = require('../util/database');
-module.exports = class Product{
-  constructor(id, title, imageUrl, price, description) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.price = price;
-    this.description = description;
-  }
+const Sequelize = require('sequelize');
 
-  save() {
-    return db.execute('INSERT INTO products (title, imageUrl, price, description) VALUES (?, ?, ?, ?)', [this.title, this.imageUrl, this.price, this.description]);
-  }
+const sequelize = require('../util/database');
 
-  static deleteById(id) {
-    
+const Product = sequelize.define('product', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  title: Sequelize.STRING,
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false
   }
+});
 
-  static fetchAll() {
-    return db.execute('SELECT * FROM products');
-  }
-
-  static findById(id) {
-    return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);
-  }
-}
+module.exports = Product;
