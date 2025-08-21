9f9c1ff606744a1d14730e4315bae463645ef40b
indahtheresia <indahtheresiatheh29@gmail.com>
2025-08-21 15:59:32 +0800

    creating & managing a dummy user

--- app.js
+++ app.js
@@ -21,6 +21,13 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  User.findByPk(1).then(user => {
+    req.user = user;
+    next();
+  }).catch(err => console.log(err));
+})
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
@@ -29,8 +36,13 @@ app.use(page404Controllers.get404);
 Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
 User.hasMany(Product);
 
-sequelize.sync({force: true}).then(result => {
+sequelize.sync().then(result => {
   // console.log(result);
-  app.listen(3000);
-}).catch(err => console.log(err));
+  return User.findByPk(1);
+}).then(user =>{
+  if (!user) {
+    return User.create({name: 'Indah', email:'indah@email.com'});
+  }
+  return user
+}).then(result => app.listen(3000)).catch(err => console.log(err));
 
