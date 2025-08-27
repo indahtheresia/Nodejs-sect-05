8e156664a5d87f16de7c17797fcc89fbaa94d8af
indahtheresia <indahtheresiatheh29@gmail.com>
2025-08-27 09:45:11 +0800

    fixing a bug

--- controllers/shop.js
+++ controllers/shop.js
@@ -46,6 +46,7 @@ exports.postCart = (req, res, next) => {
     return req.user.addToCart(product)
   }).then(result => {
     console.log(result);
+    res.redirect('/cart');
   }).catch(err => console.log(err));
   // let fetchedCart;
   // let newQuantity = 1
