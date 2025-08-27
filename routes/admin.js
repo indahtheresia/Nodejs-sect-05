791be5020f1786d1e641983e41374ab3a10bd8ae
indahtheresia <indahtheresiatheh29@gmail.com>
2025-08-27 15:48:02 +0800

    updating products

--- routes/admin.js
+++ routes/admin.js
@@ -10,12 +10,12 @@ router.get("/add-product", adminController.getAddProduct);
 
 router.post("/add-product", adminController.postAddProduct);
 
-// router.post("/edit-product", adminController.postEditProduct);
+router.post("/edit-product", adminController.postEditProduct);
 
-// router.get("/edit-product/:productId", adminController.getEditProduct);
+router.get("/edit-product/:productId", adminController.getEditProduct);
 
 // router.post("/delete-product", adminController.postDeleteProduct);
 
-// router.get("/products", adminController.getAdminProducts);
+router.get("/products", adminController.getAdminProducts);
 
 module.exports = router;
