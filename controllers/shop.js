d4bb693e9f25f29a263daa8af2a7a62271b2f7c1
indahtheresia <indahtheresiatheh29@gmail.com>
2025-08-28 12:37:48 +0800

    storing all order related data

--- controllers/shop.js
+++ controllers/shop.js
@@ -89,7 +89,7 @@ exports.postCartDeleteProduct = (req, res, next) => {
 exports.postOrder = (req, res, next) => {
   req.user.populate('cart.items.productId').then(user => {
     const products = user.cart.items.map(i => {
-      return {quantity: i.quantity, product: i.productId};
+      return {quantity: i.quantity, product: {...i.productId._doc}};
     })
     const order = new Order({
       user: {
