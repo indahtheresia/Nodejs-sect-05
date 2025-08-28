89d1058d7f4b81a2bebf26af9f1570cd20faee63
indahtheresia <indahtheresiatheh29@gmail.com>
2025-08-28 14:46:02 +0800

    creating the login form

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ require('dotenv').config();
 const page404Controllers = require('./controllers/404');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 const User = require('./models/user');
 
 const app = express();
@@ -29,6 +30,7 @@ app.use((req, res, next) => {
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(page404Controllers.get404);
 
