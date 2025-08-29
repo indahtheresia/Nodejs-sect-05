5fe8cc718187f30d3ec780008212fde68e04c3fa
indahtheresia <indahtheresiatheh29@gmail.com>
2025-08-29 11:45:52 +0800

    using mongodb to store sessions

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 require('dotenv').config();
 // console.log(process.env.DATABASE_PASSWORD);
@@ -15,6 +16,10 @@ const authRoutes = require('./routes/auth');
 const User = require('./models/user');
 
 const app = express();
+const store = new MongoDBStore({
+  uri: process.env.MONGODB_URL,
+  collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -24,7 +29,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
   secret: 'my secret',
   resave: false,
-  saveUninitialized: false
+  saveUninitialized: false,
+  store: store
 }));
 
 app.use((req, res, next) => {
