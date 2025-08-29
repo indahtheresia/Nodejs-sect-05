7eeeb5025691c3f4771a5289f159e8b7d46e906a
indahtheresia <indahtheresiatheh29@gmail.com>
2025-08-29 11:19:37 +0800

    using the session middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,11 @@
 exports.getLogin = (req, res, next) => {
   // const isLoggedIn = req.get('Cookie').split(';')[1].split('=')[1];
+  console.log(req.session.isLoggedIn);
   res.render('auth/login', { title: 'Login', path: '/login', isAuthenticated: false });
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader('Set-Cookie', 'loggedIn=true; HttpOnly');
+  req.session.isLoggedIn = true;
+  // res.setHeader('Set-Cookie', 'loggedIn=true; HttpOnly');
   res.redirect('/');
 }
