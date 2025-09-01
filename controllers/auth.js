43ed932f764108c0b873711e14849916dddd2b17
indahtheresia <indahtheresiatheh29@gmail.com>
2025-09-01 16:40:27 +0800

    adding the signin functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -13,12 +13,23 @@ exports.getSignup = (req, res, next) => {
 }
 
 exports.postLogin = (req, res, next) => {
-  User.findById('68af1a28dd2c3928a3974eb4').then(user => {
-    req.session.isLoggedIn = true;
-    req.session.user = user;
-    req.session.save(err => {
-      console.log(err);
-      res.redirect('/');
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({email: email}).then(user => {
+    if (!user) {
+      return res.redirect('/login');
+    }
+    bcrypt.compare(password, user.password)
+    .then(doMatch => {
+      if (doMatch) {
+        req.session.isLoggedIn = true;
+        req.session.user = user;
+        return req.session.save(err => {
+        console.log(err);
+        res.redirect('/');
+      });
+      }
+      res.redirect('/login');
     })
   }).catch(err => console.log(err));
   // res.setHeader('Set-Cookie', 'loggedIn=true; HttpOnly');
