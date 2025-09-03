4b861b254582be71bd556d2b6c509346c34b0062
indahtheresia <indahtheresiatheh29@gmail.com>
2025-09-03 18:06:56 +0800

    validating product addition

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,8 +1,10 @@
+const validationResult = require('express-validator').validationResult;
+
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
   // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
-  res.render('admin/edit-product', { title: 'Add Product', path:'/admin/add-product', editing: false, isAuthenticated: req.session.isLoggedIn });
+  res.render('admin/edit-product', { title: 'Add Product', path:'/admin/add-product', editing: false, hasError: false, errorMessage: null });
 }
 
 exports.postAddProduct = (req, res, next) => {
@@ -10,6 +12,16 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).render('admin/edit-product', { title: 'Add Product', path:'/admin/add-product', editing: false, hasError: true, product: {
+      title: title,
+      imageUrl: imageUrl,
+      price: price,
+      description: description
+    }, errorMessage: errors.array()[0].msg });
+  }
+
   const product = new Product({title: title, price: price, description: description, imageUrl: imageUrl, userId: req.user});
   
   product.save().then(result => {
@@ -30,7 +42,7 @@ exports.getEditProduct = (req, res, next) => {
     if (!product) {
       return res.redirect('/');
     }
-    res.render('admin/edit-product', { title: 'Edit Product', path:'/admin/edit-product', editing: editMode, product: product, isAuthenticated: req.session.isLoggedIn });
+    res.render('admin/edit-product', { title: 'Edit Product', path:'/admin/edit-product', editing: editMode, product: product, hasError: false, errorMessage: null });
   }).catch(err => console.log(err));
 }
 
