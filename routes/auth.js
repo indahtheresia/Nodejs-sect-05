b0135f8eb2a4479ebb0f3f3a4f1f5e4725fa8b9a
indahtheresia <indahtheresiatheh29@gmail.com>
2025-09-03 12:21:11 +0800

    add validators for password field in post sign up

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const check = require('express-validator').check;
+const body = require('express-validator').body;
 
 const authController = require('../controllers/auth');
 
@@ -13,12 +14,15 @@ router.post('/logout', authController.postLogout);
 
 router.get('/signup', authController.getSignup);
 
-router.post('/signup', check('email').isEmail().withMessage('Please enter a valid email.').custom((value, {req}) => {
-  if (value === 'test@test.com') {
-    throw new Error('This email address if forbidden.');
-  }
-  return true;
-}), authController.postSignup);
+router.post('/signup', [
+    check('email').isEmail().withMessage('Please enter a valid email.').custom((value, {req}) => {
+    if (value === 'test@test.com') {
+      throw new Error('This email address if forbidden.');
+    }
+    return true;
+  }),
+  body('password', 'Please enter a password with only numbers and text and at least 5 characters.').isLength({min: 5}).isAlphanumeric()
+], authController.postSignup);
 
 router.get('/reset', authController.getResetPassword);
 
