235374088f0069c93e13a724358996535cacb3f0
indahtheresia <indahtheresiatheh29@gmail.com>
2025-09-03 12:25:43 +0800

    checking for field equality

--- routes/auth.js
+++ routes/auth.js
@@ -21,7 +21,13 @@ router.post('/signup', [
     }
     return true;
   }),
-  body('password', 'Please enter a password with only numbers and text and at least 5 characters.').isLength({min: 5}).isAlphanumeric()
+  body('password', 'Please enter a password with only numbers and text and at least 5 characters.').isLength({min: 5}).isAlphanumeric(),
+  body('confirmPassword').custom((value, { req }) => {
+    if (value !== req.body.password) {
+      throw new Error('Passwords have to match!');
+    }
+    return true;
+  })
 ], authController.postSignup);
 
 router.get('/reset', authController.getResetPassword);
